c8531bb291d6ec58549d72d02e95ed8929fd8045
Maryann <mwikali119@gmail.com>
2022-06-04 11:11:35 +0300

    Installed dependencies and ran migrations

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://mwiks-dev:1455@localhost/fyurr'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
